6465fcca6b29644407caff7b8aefdad7e7462748
Siddharth Dubey <siddharth.dubey5815@gmail.com>
2025-09-19 05:32:38 +0530

    providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,14 +7,6 @@ terraform {
       version = ">= 4.0"
     }
   }
-
-  # Optional remote state (recommended in real projects)
-  # backend "s3" {
-  #   bucket = "your-tf-state-bucket"
-  #   key    = "nomad/terraform.tfstate"
-  #   region = "ap-south-1"
-  #   encrypt = true
-  # }
 }
 
 provider "aws" {
